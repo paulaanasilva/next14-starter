526574352526cae5fbb3ff05b75a523bb1f5232d
NOTEDELLANA\anap <de.anapaulasilva@gmail.com>
2024-08-16 15:30:31 -0300

    Fetch data from local data.js

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,27 +1,51 @@
 import styles from './singlePost.module.css';
 import Image from 'next/image';
+import PostUser from '@/components/postUser/PostUser';
+import { Suspense } from 'react';
+import { getPost } from '@/lib/data';
 
-const SlugBlogPage = () => {
+/*
+const getData = async (slug) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`, { next: { revalidate: 3600 } });
+
+    if (!res.ok) {
+        throw new Error('something went wrong');
+    }
+
+    return res.json();
+};
+*/
+
+
+const SlugBlogPage = async ({ params }) => {
+
+    const { slug } = params;
+
+    //Fetch data from API
+    //const post = await getData(slug);
+
+    //Fetch data from local data.js
+    const post = await getPost(slug);
+
     return (
         <div className={styles.container}>
             <div className={styles.imgContainer}>
-                <Image src="/contact.png" alt="Placeholder" width={300} height={300}/>
+                <Image src="/contact.png" alt="Placeholder" width={300} height={300} />
             </div>
             <div className={styles.textContainer}>
-                <h1 className={styles.title}>Blog Title</h1>
+                <h1 className={styles.title}>{post?.title}</h1>
                 <div className={styles.detail}>
-                    <Image src="/favicon.ico" alt="Avatar" width={50} height={50}/>
-                    <div className={styles.detailText}>
-                        <span className={styles.detailTitle}>Author</span>
-                        <span className={styles.detailValue}>Terry Jeferson</span>
-                    </div>
+                    <Image src="/favicon.ico" alt="Avatar" width={50} height={50} />
+                    {post && <Suspense fallback={<div>Loading...</div>}>
+                        <PostUser userId={post?.userId} />
+                    </Suspense>}
                     <div className={styles.detailText}>
                         <span className={styles.detailTitle}>Published</span>
                         <span className={styles.detailValue}>01.01.2024</span>
                     </div>
                 </div>
                 <div className={styles.content}>
-                Hoje, em Santa Catarina, o clima está ameno, com sol entre nuvens, propício para atividades ao ar livre e turismo.
+                    {post?.body}
                 </div>
             </div>
         </div>
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,24 +1,35 @@
 import PostCard from "@/components/postCard/PostCard";
 import styles from "./blog.module.css";
+import { getPosts } from "@/lib/data";
 
-const BlogPage = () => {
+/*
+const getData = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts', {next:{revalidate:3600}});
+
+    if (!res.ok) {
+        throw new Error('something went wrong');
+    }
+
+    return res.json();
+};
+*/
+
+const BlogPage = async () => {
+
+    //Fetch data from API
+    //  const posts = await getData();
+
+    //Fetch data from local data.js
+    const posts = await getPosts();
+
     return (
         <div className={styles.container}>
-            <div className={styles.post}>
-                <PostCard />
-            </div>
-            <div className={styles.post}>
-                <PostCard />
-            </div>
-            <div className={styles.post}>
-                <PostCard />
-            </div>
-            <div className={styles.post}>
-                <PostCard />
-            </div>
-            <div className={styles.post}>
-                <PostCard />
-            </div>
+            {posts.map((post) => (
+                <div className={styles.post} key={post.id}>
+                    <PostCard post={post} />
+                </div>
+            ))}
+
         </div>
     );
 };
